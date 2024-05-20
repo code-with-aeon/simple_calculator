d542e93e19e7925c81cd40256ebd18bba60af429
Gabriel Ibañez <ibanezgabrielx152@gmail.com>
2024-05-20 14:09:45 +0800

    Create Basic Calculator

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+const number = document.querySelector('.number');
+const operator = document.querySelector('.operator');
+const resultLast = document.querySelector('.result');
+const clear = document.querySelector('.clear');
+const del = document.querySelector('.delete');
