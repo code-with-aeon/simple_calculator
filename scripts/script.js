32e1fb21c06acbf1b57560708d5d9b92b741cdd7
Gabriel Ibañez <ibanezgabrielx152@gmail.com>
2024-06-02 23:21:38 +0800

    modified scripts
    just modified some variables

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const number = document.querySelector('.number');
 const operator = document.querySelector('.operator');
-const resultLast = document.querySelector('.result');
 const clear = document.querySelector('.clear');
 const del = document.querySelector('.delete');
+const uname = document.querySelector('#_uname');
+const result = document.querySelector('#result');
